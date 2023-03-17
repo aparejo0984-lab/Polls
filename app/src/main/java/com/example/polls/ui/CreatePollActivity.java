46d69b68545bf1f7f4a9f0725e1029e319806dce
Anna Lou Parejo <aparejo0984@gmail.com>
2023-03-17 10:20:25 -0400

    Delete changes on the UI

--- app/src/main/java/com/example/polls/ui/CreatePollActivity.java
+++ app/src/main/java/com/example/polls/ui/CreatePollActivity.java
@@ -1,13 +1,20 @@
 package com.example.polls.ui;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.example.polls.R;
 
 public class CreatePollActivity extends AppCompatActivity {
+
+    private AlertDialog dialogDeletePoll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +23,32 @@ public class CreatePollActivity extends AppCompatActivity {
 
         ImageView imageBack = findViewById(R.id.imageBack);
         imageBack.setOnClickListener(v -> onBackPressed());
+
+        ImageView imageDelete = findViewById(R.id.imageDelete);
+        imageDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDeleteNoteDialog();
+            }
+        });
+    }
+
+    private void showDeleteNoteDialog() {
+        if(dialogDeletePoll == null) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(CreatePollActivity.this);
+            View view = LayoutInflater.from(this)
+                    .inflate(R.layout.layout_delete,
+                            (ViewGroup) findViewById(R.id.layoutDeleteNoteContainer));
+
+            builder.setView(view);
+            dialogDeletePoll = builder.create();
+
+            if(dialogDeletePoll.getWindow() != null) {
+                dialogDeletePoll.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+            }
+            view.findViewById(R.id.textDeleteCancel).setOnClickListener(v -> dialogDeletePoll.dismiss());
+        }
+
+        dialogDeletePoll.show();
     }
 }
